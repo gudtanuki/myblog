0e9ef6d66cd9e135717bd00daa291117c4aaeed6
郷田杏奈 <a_gouda@funteam.co.jp>
2019-06-26 16:12:23 +0900

    エラーテスト

--- src/resources/views/posts/show.blade.php
+++ src/resources/views/posts/show.blade.php
@@ -96,56 +96,56 @@
 </div>
 <script>
 
-$(function() {
-    $('.like-post').on('click', '.add-like', function() {
-        $.ajaxSetup({
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
-        });
-        $.ajax({
-            url: "{{ action('PostController@like_add') }}",
-            type: 'POST',
-            data: 'post'
-        })
-        // Ajaxリクエストが成功した場合
-        .done(function(data) {
-            // console.log(data);
-            $('.add-like').remove();
-            $('.like-post').prepend('<i class="fas fa-heart delete-like"></i>');
-            $('span').removeClass('color-black');
-            $('span').addClass('color-red');
-            $('span').text(data);
+// $(function() {
+//     $('.like-post').on('click', '.add-like', function() {
+//         $.ajaxSetup({
+//             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+//         });
+//         $.ajax({
+//             url: "{{ action('PostController@like_add') }}",
+//             type: 'POST',
+//             data: 'post'
+//         })
+//         // Ajaxリクエストが成功した場合
+//         .done(function(data) {
+//             // console.log(data);
+//             $('.add-like').remove();
+//             $('.like-post').prepend('<i class="fas fa-heart delete-like"></i>');
+//             $('span').removeClass('color-black');
+//             $('span').addClass('color-red');
+//             $('span').text(data);
 
-        })
-        // Ajaxリクエストが失敗した場合
-        .fail(function(data) {
-            console.log('ng');
-        });
-    });
+//         })
+//         // Ajaxリクエストが失敗した場合
+//         .fail(function(data) {
+//             console.log('ng');
+//         });
+//     });
 
-    $('.like-post').on('click', '.delete-like', function() {
-        $.ajaxSetup({
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
-        });
-        $.ajax({
-            url: "{{ action('PostController@like_destroy') }}",
-            type: 'POST',
-            data: 'post'
-        })
-        // Ajaxリクエストが成功した場合
-        .done(function(data) {
-            // console.log(data);
-            $('.delete-like').remove();
-            $('.like-post').prepend('<i class="far fa-heart add-like"></i>');
-            $('span').removeClass('color-red');
-            $('span').addClass('color-black');
-            $('span').text(data);
+//     $('.like-post').on('click', '.delete-like', function() {
+//         $.ajaxSetup({
+//             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+//         });
+//         $.ajax({
+//             url: "{{ action('PostController@like_destroy') }}",
+//             type: 'POST',
+//             data: 'post'
+//         })
+//         // Ajaxリクエストが成功した場合
+//         .done(function(data) {
+//             // console.log(data);
+//             $('.delete-like').remove();
+//             $('.like-post').prepend('<i class="far fa-heart add-like"></i>');
+//             $('span').removeClass('color-red');
+//             $('span').addClass('color-black');
+//             $('span').text(data);
 
-        })
-        // Ajaxリクエストが失敗した場合
-        .fail(function(data) {
-            console.log('ng');
-        });
-    });
-});
+//         })
+//         // Ajaxリクエストが失敗した場合
+//         .fail(function(data) {
+//             console.log('ng');
+//         });
+//     });
+// });
 </script>
 @endsection
